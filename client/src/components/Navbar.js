d6c697ddf917102d7c1e3696264fcee36b65275d
Patrick Hull <patrick9hull4@gmail.com>
2023-05-30 11:18:54 -0700

    fix: navbar logo alt text was from another project of mine

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css"
 export default function Navbar() {
   return (
     <div className="header-container">
-      <img className="logo-img" src={logo} alt="suncheck logo"></img>
+      <img className="logo-img" src={logo} alt="divergent logo"></img>
     </div>
   );
 }
